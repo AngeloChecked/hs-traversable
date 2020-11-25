b42cac26df2dec5adeae1402b41d6bc088001668
Angelo Ceccato <angelo.ceccato@it.clara.net>
2020-11-25 23:28:57 +0100

    add some iteresting stuff and improve stack test confing

--- src/Traversable.hs
+++ src/Traversable.hs
@@ -1,3 +1,30 @@
 module Traversable where
 
 foo = 1
+
+data Record
+
+myData :: [String]
+myData = undefined 
+
+myFunc :: String -> IO Record
+myFunc = undefined
+
+wrong :: [IO Record]
+wrong = fmap myFunc myData
+
+right :: IO [Record]
+right = traverse myFunc myData
+
+-- t (f b) -> f (t b)
+
+doubleFmap :: (Functor f1, Functor f2) => (a -> b) -> f1 (f2 a) -> f1 (f2 b)
+doubleFmap = (fmap . fmap)
+
+dfmapOfsumJust :: (Foldable t, Num b) => t b -> Maybe b
+dfmapOfsumJust = doubleFmap sum Just
+
+just :: ((->) a) (Maybe a)
+just = \a -> Just a
+
+
--- test/TraversableSpec.hs
+++ test/TraversableSpec.hs
@@ -5,7 +5,9 @@ import Traversable
 
 spec :: Spec
 spec = 
-    describe "" $
-        it "" $
-            foo `shouldBe` 1
+    describe "Traversable" $ do
+        it "double fmap of sum Just" $ do
+            (dfmapOfsumJust [1,2,3]) `shouldBe` (Just 6) 
+            ((fmap . fmap) sum Just [1,2,3]) `shouldBe` (Just 6)
+            (fmap sum $ Just [1,2,3]) `shouldBe` (Just 6)
 
